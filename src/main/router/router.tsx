85de0aae27749ae570a02c0369a119c5e668409f
felipe-zg <felipe_zeba@outlook.com>
2021-02-23 16:57:27 -0300

    refactor: add factory method to make TodoList page on router

--- src/main/router/router.tsx
+++ src/main/router/router.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import { TodoList } from 'presentation/pages';
+import { makeTodoList } from 'main/factories/pages/todo-list/todo-list-factory';
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={TodoList} />
+        <Route path="/" component={makeTodoList} />
       </Switch>
     </BrowserRouter>
   );
