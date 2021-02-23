266b5b0ac43c65e16162db1d9c4d5c89092c6f4f
felipe-zg <felipe_zeba@outlook.com>
2021-02-23 11:27:45 -0300

    feat: ensure AxiosHttpClient.get returns correct value on failure

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,9 +1,18 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { HttpGetClient, HttpGetParams, HttpResponse } from 'data/protocols/http';
 
 export class AxiosHttpClient<R = unknown> implements HttpGetClient<R> {
   async get(params: HttpGetParams): Promise<HttpResponse<R>> {
-    const axiosResponse = await axios.get(params.url);
+    let axiosResponse: AxiosResponse<R>;
+    try {
+      axiosResponse = await axios.get(params.url);
+    } catch (error) {
+      axiosResponse = error.response;
+    }
+    return this.adapt(axiosResponse);
+  }
+
+  private adapt(axiosResponse: AxiosResponse): HttpResponse<R> {
     return {
       statusCode: axiosResponse.status,
       body: axiosResponse.data,
