1323bf34b813bfbbb5052476e4ed32334e2bc22c
felipe-zg <felipe_zeba@outlook.com>
2021-02-22 23:01:14 -0300

    feat: ensure RemoteLoadTodoList throws UnexpectedError on 500

--- src/data/use-cases/load-todo-list/load-todo-list.ts
+++ src/data/use-cases/load-todo-list/load-todo-list.ts
@@ -1,15 +1,24 @@
-import { HttpGetClient } from 'data/protocols/http';
+import { HttpGetClient, HttpStatusCode } from 'data/protocols/http';
+import { UnexpectedError } from 'domain/errors';
+import { TodoModel } from 'domain/models/todo-model';
 
 export class RemoteLoadTodoList {
   constructor(
     private readonly url: string,
-    private readonly httpGetClient: HttpGetClient
+    private readonly httpGetClient: HttpGetClient<TodoModel[]>
   ) {
     this.url = url;
     this.httpGetClient = httpGetClient;
   }
 
   async loadAll(): Promise<void> {
-    await this.httpGetClient.get({ url: this.url });
+    const httpResponse = await this.httpGetClient.get({ url: this.url });
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:
+        break;
+      default:
+        throw new UnexpectedError();
+    }
   }
 }
