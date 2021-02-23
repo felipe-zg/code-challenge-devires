000a6043b2a75ff8c8be90ad8506e4c33ecab7e0
felipe-zg <felipe_zeba@outlook.com>
2021-02-22 22:13:15 -0300

    refactor: move sut create to a factory method

--- src/data/use-cases/load-todo-list/remote-load-todo-list.spec.ts
+++ src/data/use-cases/load-todo-list/remote-load-todo-list.spec.ts
@@ -3,11 +3,24 @@ import faker from 'faker';
 import { HttpGetClientSpy } from 'data/test';
 import { RemoteLoadTodoList } from './load-todo-list';
 
+type SutTypes = {
+  sut: RemoteLoadTodoList;
+  httpGetClientSpy: HttpGetClientSpy;
+};
+
+const makeSut = (url = faker.internet.url()): SutTypes => {
+  const httpGetClientSpy = new HttpGetClientSpy();
+  const sut = new RemoteLoadTodoList(url, httpGetClientSpy);
+  return {
+    sut,
+    httpGetClientSpy,
+  };
+};
+
 describe('RemoteLoadTodoList', () => {
   it('should call HttpGetClient with correct url', async () => {
     const url = faker.internet.url();
-    const httpGetClientSpy = new HttpGetClientSpy();
-    const sut = new RemoteLoadTodoList(url, httpGetClientSpy);
+    const { sut, httpGetClientSpy } = makeSut(url);
     await sut.loadAll();
     expect(httpGetClientSpy.url).toBe(url);
   });
