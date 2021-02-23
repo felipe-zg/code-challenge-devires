fb42f24f4a386cceb7417d0658c9a1cc8c4220b3
felipe-zg <felipe_zeba@outlook.com>
2021-02-22 23:01:25 -0300

    test: ensure RemoteLoadTodoList throws UnexpectedError on 500

--- src/data/use-cases/load-todo-list/remote-load-todo-list.spec.ts
+++ src/data/use-cases/load-todo-list/remote-load-todo-list.spec.ts
@@ -2,14 +2,17 @@ import faker from 'faker';
 
 import { HttpGetClientSpy } from 'data/test';
 import { RemoteLoadTodoList } from './load-todo-list';
+import { HttpStatusCode } from 'data/protocols/http';
+import { TodoModel } from 'domain/models/todo-model';
+import { UnexpectedError } from 'domain/errors';
 
 type SutTypes = {
   sut: RemoteLoadTodoList;
-  httpGetClientSpy: HttpGetClientSpy;
+  httpGetClientSpy: HttpGetClientSpy<TodoModel[]>;
 };
 
 const makeSut = (url = faker.internet.url()): SutTypes => {
-  const httpGetClientSpy = new HttpGetClientSpy();
+  const httpGetClientSpy = new HttpGetClientSpy<TodoModel[]>();
   const sut = new RemoteLoadTodoList(url, httpGetClientSpy);
   return {
     sut,
@@ -23,5 +26,14 @@ describe('RemoteLoadTodoList', () => {
     const { sut, httpGetClientSpy } = makeSut(url);
     await sut.loadAll();
     expect(httpGetClientSpy.url).toBe(url);
+  });
+
+  it('should throw UnexpectedError if HttpGetClient returns 500', async () => {
+    const { sut, httpGetClientSpy } = makeSut();
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.serverError,
+    };
+    const promise = sut.loadAll();
+    await expect(promise).rejects.toThrow(new UnexpectedError());
   });
 });
