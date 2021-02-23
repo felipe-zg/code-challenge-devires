0e340232a2ccb1bbacb88243cd073e7818fb160f
felipe-zg <felipe_zeba@outlook.com>
2021-02-22 21:46:42 -0300

    feat: ensure RemoteLoadTodoList callHttpGetClient with correct url

---
+++ src/data/use-cases/load-todo-list/load-todo-list.ts
@@ -0,0 +1,15 @@
+import { HttpGetClient } from 'data/protocols/http';
+
+export class RemoteLoadTodoList {
+  constructor(
+    private readonly url: string,
+    private readonly httpGetClient: HttpGetClient
+  ) {
+    this.url = url;
+    this.httpGetClient = httpGetClient;
+  }
+
+  async loadAll(): Promise<void> {
+    await this.httpGetClient.get({ url: this.url });
+  }
+}
