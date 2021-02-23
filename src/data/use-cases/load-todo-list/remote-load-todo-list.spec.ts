9572251be7c5ad35bd73bb4332d81c42bab6c920
felipe-zg <felipe_zeba@outlook.com>
2021-02-22 23:02:39 -0300

    test: ensure RemoteLoadTodoList throws UnexpectedError on 404

--- src/data/use-cases/load-todo-list/remote-load-todo-list.spec.ts
+++ src/data/use-cases/load-todo-list/remote-load-todo-list.spec.ts
@@ -36,4 +36,13 @@ describe('RemoteLoadTodoList', () => {
     const promise = sut.loadAll();
     await expect(promise).rejects.toThrow(new UnexpectedError());
   });
+
+  it('should throw UnexpectedError if HttpGetClient returns 404', async () => {
+    const { sut, httpGetClientSpy } = makeSut();
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.notFound,
+    };
+    const promise = sut.loadAll();
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
 });
