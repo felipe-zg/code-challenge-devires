6e0a8c617be041d49fcd33c40caf7ed5ae790c25
felipe-zg <felipe_zeba@outlook.com>
2021-02-23 22:25:25 -0300

    feat: add functionalitty to create new todo and add it to redux

--- src/presentation/pages/Todo-list/index.tsx
+++ src/presentation/pages/Todo-list/index.tsx
@@ -1,30 +1,71 @@
-import React, { useEffect, useState } from 'react';
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { LoadTodoList } from 'domain/use-cases/load-todo-list';
-import { TodoModel } from 'domain/models/todo-model';
 import { Load, Todo } from 'presentation/components';
+
+import { selectTodos, createTodoActionCreator } from 'store/ducks/todos.duck';
 
 type Props = {
   loadTodoList: LoadTodoList;
 };
 
 const TodoList: React.FC<Props> = ({ loadTodoList }: Props) => {
-  const [list, setList] = useState({
-    todos: [] as TodoModel[] | undefined,
-  });
+  const dispatch = useDispatch();
+  const todos = useSelector(selectTodos);
+  const [newTodoTitle, setNewTodoTitle] = useState<string>('');
+  const [newTodoDescription, setNewTodoDescription] = useState<string>('');
 
   useEffect(() => {
-    loadTodoList.loadAll().then((todos) => setList({ todos }));
+    loadTodoList.loadAll();
   }, [loadTodoList]);
+
+  const handleCreateNewTodo = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    if (!newTodoTitle.length || !newTodoDescription.length) return;
+
+    dispatch(
+      createTodoActionCreator({
+        title: newTodoTitle,
+        description: newTodoDescription,
+      })
+    );
+    setNewTodoTitle('');
+    setNewTodoDescription('');
+  };
+
+  const handleNewInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    if (e.target.id === 'new-todo-title') {
+      setNewTodoTitle(e.target.value);
+      return;
+    }
+    setNewTodoDescription(e.target.value);
+  };
 
   return (
     <div>
       <h2>Todolist</h2>
-      {list.todos?.length ? (
-        list.todos.map((item) => <Todo key={item.id} todo={item} />)
+      {todos?.length ? (
+        todos.map((item) => <Todo key={item.id} todo={item} />)
       ) : (
         <Load />
       )}
+      <h1>Novo item</h1>
+      <form onSubmit={handleCreateNewTodo}>
+        <label htmlFor="new-todo-title">Titulo:</label>
+        <input
+          onChange={handleNewInputChange}
+          id="new-todo-title"
+          value={newTodoTitle}
+        />
+        <label htmlFor="new-todo-description">descrição:</label>
+        <input
+          onChange={handleNewInputChange}
+          id="new-todo-description"
+          value={newTodoDescription}
+        />
+        <button type="submit">Criar</button>
+      </form>
     </div>
   );
 };
