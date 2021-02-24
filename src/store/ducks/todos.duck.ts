3766cf74d7a2d3920e85b76c594c5dfbe4cb76e1
felipe-zg <felipe_zeba@outlook.com>
2021-02-24 13:21:01 -0300

    refacotr: change duck to make async request with thunk

--- src/store/ducks/todos.duck.ts
+++ src/store/ducks/todos.duck.ts
@@ -1,45 +1,64 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction, Dispatch } from '@reduxjs/toolkit';
 import { RootState } from 'store';
 import { TodoModel } from 'domain/models/todo-model';
-import { generateId } from 'store/utils/id-generator';
+import { makeRemoteLoadTodoList } from 'main/factories/use-cases/load-todo-list/remote-load-todo-list-factory';
 
-const todosInitialState: TodoModel[] = [];
+export interface State {
+  loading: boolean;
+  error: string | null;
+  todos: TodoModel[];
+}
+
+const todosInitialState: State = {
+  loading: false,
+  error: null,
+  todos: [],
+};
 
 const todosSlice = createSlice({
   name: 'todos',
   initialState: todosInitialState,
   reducers: {
-    create: {
-      reducer: (
-        state,
-        {
-          payload,
-        }: PayloadAction<{ id: number; title: string; description: string }>
-      ) => {
-        state.push(payload);
-      },
-      prepare: ({ title, description }: { title: string; description: string }) => ({
-        payload: {
-          id: generateId(), // GENERATE RANDOM ID HERE
-          title,
-          description,
-        },
-      }),
-    },
-    remove: (state, { payload }: PayloadAction<{ id: number }>) => {
-      const index = state.findIndex((todo) => todo.id === payload.id);
-      if (index !== -1) {
-        state.splice(index, 1);
-      }
-    },
+    setLoading: (state: State, { payload }: PayloadAction<boolean>) => ({
+      ...state,
+      loading: payload,
+    }),
+    setTodos: (state: State, { payload }: PayloadAction<TodoModel[]>) => ({
+      ...state,
+      todos: payload,
+    }),
+    setError: (state: State, { payload }: PayloadAction<string | null>) => ({
+      ...state,
+      error: payload,
+    }),
   },
 });
 
-export const {
-  create: createTodoActionCreator,
-  remove: deleteTodoActionCreator,
-} = todosSlice.actions;
+export const { setLoading, setError, setTodos } = todosSlice.actions;
 
 export default todosSlice.reducer;
+
+const loadList = (dispatch: Dispatch) => {
+  makeRemoteLoadTodoList()
+    .loadAll()
+    .then((todos) => {
+      dispatch(setError(null));
+      dispatch(setTodos(todos as TodoModel[]));
+      dispatch(setLoading(false));
+    })
+    .catch((err: Error) => {
+      dispatch(setLoading(false));
+      dispatch(setError(err.message));
+    });
+};
+
+export const getTodos = () => async (
+  dispatch: Dispatch,
+  getState: () => RootState
+) => {
+  getState();
+  dispatch(setLoading(true));
+  loadList(dispatch);
+};
 
 export const selectTodos = (state: RootState) => state.todos;
