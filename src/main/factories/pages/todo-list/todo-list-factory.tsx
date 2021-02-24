49c753c75a6d7b0a761ae4effc391d3c1fdde77b
felipe-zg <felipe_zeba@outlook.com>
2021-02-24 13:22:13 -0300

    refacotr: remove loadTodoList dependency from TodoList

--- src/main/factories/pages/todo-list/todo-list-factory.tsx
+++ src/main/factories/pages/todo-list/todo-list-factory.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { TodoList } from '../../../../presentation/pages';
-import { makeRemoteLoadTodoList } from 'main/factories/use-cases/load-todo-list/remote-load-todo-list-factory';
 
 export const makeTodoList: React.FC = () => {
-  return <TodoList loadTodoList={makeRemoteLoadTodoList()} />;
+  return <TodoList />;
 };
